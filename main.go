3d0a9531ea20015eaff7fdef77f40aff96f4f613
AlariCode <antonlarichev@gmail.com>
2024-04-10 11:16:54 +0300

    Работа с массивами

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
+import "fmt"
+
 func main() {
-	transactions := [3]int{5, 10, -7}
-	banks = [2]string{"Тинькофф", "Альфа"}
+	transactions := [3]int{1, 2, 3}
+	banks := [2]string{}
+
+	fmt.Println(transactions[1])
+	banks[0] = "Тинькофф"
+	fmt.Println(banks)
 }
